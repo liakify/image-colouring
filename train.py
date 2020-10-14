618a43b3015958287b22b32101a8ffd8964794e3
liakify <liakify@gmail.com>
2020-10-14 08:43:09 +0000

    Train MSE and CIE model on 10% of images with 100 epochs

--- train.py
+++ train.py
@@ -10,34 +10,43 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 
-ids = data.getImageIds(0.01)
+ids = data.getImageIds(0.05)
 trainIds, testIds = train_test_split(ids, test_size=0.2, random_state=42)
 print("Len of trainIds:", len(trainIds))
 print("Len of testIds:", len(testIds))
 
-np.save("npy/CIE94_train_1_20_32_10000", trainIds)
-np.save("npy/CIE94_test_1_20_32_10000", testIds)
+np.save("npy/train", trainIds)
+np.save("npy/test", testIds)
 
 X_train, Y_train = data.loadImageData(trainIds)
 
 # Model specific code
-
+'''
 # MSE model
 Y_train /= 128
-model = models.getCIE94Model()
+model = models.getMSEModel()
 
 model.fit(x=X_train, 
     y=Y_train,
     batch_size=32,
-    epochs=10000)
+    epochs=100)
 
-model.save("models/CIEmodel_1_20_32_10000")
-
+model.save("models/MSEmodel_10_20_32_100")
+'''
 
 # Classification model
-'''
+
 bins = np.load("npy/pts_in_hull.npy")
 model = models.getClassificationModel()
+Y_train = data.batchQuantize(Y_train, bins)
+
+model.fit(x=X_train,
+    y=Y_train,
+    batch_size=32,
+    epochs=100)
+
+'''
+# Manual loop if unable to process all in RAM at once
 epochs = 100
 with tf.device("cpu:0"):
     for i in range(epochs):
@@ -49,6 +58,6 @@ with tf.device("cpu:0"):
                 y=Y_train,
                 batch_size=1,
                 epochs=1)
-
-model.save("models/ClassificationModel")
 '''
+model.save("models/ClassificationModel_10_20_32_100")
+
